5c99a73b5a4feddb537f35c2a22276d51509e131
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-01-15 13:17:57 +0100

    offers next digit upon error instead of quitting
    mostly stateless

--- server/stateless.sh
+++ server/stateless.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-trap 'rm -f /tmp/pi' HUP INT TERM KILL EXIT
+trap '{ rm -f /tmp/pi; exit 0;}' HUP INT TERM KILL EXIT
 
 mkfifo /tmp/pi 
 
 exec 9<>/tmp/pi
 pi ${1:-51} >&9
 
+i=0
+
 echo 'PI:'
 while read -n1 -u9 p
 do
-
   if [[ "x${p}" == "x" ]]
   then
     echo -e '\nNice'
@@ -18,15 +19,26 @@ do
     exit 0
   fi
 
-  read -n1 -u0 -s d
-  if [[ "${d}" == "${p}" ]]
-  then
-    echo -n "${p}"
-  else
-    echo -e "\n${d} -> ${p}"
-    exec 9>&-
-    exit 1
-  fi
+  while read -n1 -u0 -s d
+  do
+    if [[ "${d}" == "${p}" ]]
+    then
+      echo -n "${p}"
+      break
+    else
 
+      if [[ "${d}" == "q" ]]
+      then
+        echo -e "\nBye."
+        exit 0
+      fi
+
+      echo -e "${d} -> ${p}"
+      pi ${i} | head -c -2 # utf8-yerself 
+
+    fi
+  done
+
+  (( i++ ))
 done 
 
