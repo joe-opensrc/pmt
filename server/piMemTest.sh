82927d6fd73dc36dfce9f9841c1faaad4f92a94a
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-02-11 17:14:56 +0100

    arg handling + adds function
    adds assertInteger
    checks positional args:
     - $1: number of decimal places
     - $2: start/offset at which to start matching

--- server/piMemTest.sh
+++ server/piMemTest.sh
@@ -2,7 +2,6 @@
 
 trap '{ rm -f /tmp/pi; exit 0;}' HUP INT TERM KILL EXIT
 
-mkfifo /tmp/pi 
 function assertInteger(){
   if ! [[ ${1} =~ ^[0-9]+$ ]]
   then
@@ -11,12 +10,48 @@ function assertInteger(){
   fi
 }
 
-exec 9<>/tmp/pi
-pi ${1:-51} >&9
+# for arg in "${@}"
+# do
+#   assertInteger ${1}
+#   shift
+# done
+
+# num places
+np=6 #1
+# start place
+sp=1
 
 # state; but if you're capable of memorizing pi upto MAX_INTEGER
-# this is the least of your worries
+# this is the least of your worries :)
 i=0
+e=0
+
+
+if [[ ${#} -gt 0 ]]
+then
+    assertInteger ${1}
+    np=$(( ${1} + 1 ))
+fi
+
+  mkfifo /tmp/pi 
+  exec 9<>/tmp/pi
+  pi ${np} >&9
+
+  if [[ -n ${2} ]]
+  then
+    assertInteger ${2}
+    sp=$(( ${2} + 2 ))
+    if [[ ${2} -lt $(( ${np} -1 )) ]]
+    then
+      read -n ${sp} -u9 
+      i=${sp} 
+    else
+      echo -e "${0} <num_decimals> <start_offset>\nE: start_offset !< num_decimals" >&2
+      exit 9
+    fi
+  fi
+
+
 
 echo 'PI (q-quit):'
 while read -n1 -u9 p
